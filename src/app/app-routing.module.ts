263de335dce0a47339becbefa30f9b0399d8b1ff
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-04-19 16:16:14 +0700

    edit category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { CandidateComponent } from './candidate/candidate.component';
 import { CategoryComponent } from './category/category.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 import { HomeComponent } from './home/home.component';
+import { JobComponent } from './job/job.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -9,6 +15,12 @@ const routes: Routes = [
     path: '',
     children: [
       {path: 'category', component: CategoryComponent},
+      {path: 'product', component: ProductComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: 'candidate', component: CandidateComponent},
+      {path: 'job', component: JobComponent},
+      {path: 'feedback', component: FeedbackComponent},
+      {path: 'account', component: AccountComponent},
       
     ]
   },
